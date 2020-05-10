67affdb309947db85560f8d9303c021755cdfa80
Valera <gavrilow777@gmail.com>
2020-05-10 19:21:02 +0300

    first init

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/mnt/afe01ad8-a763-4688-94a9-8827ed472dfa/flutterSdk/flutter"
+export "FLUTTER_APPLICATION_PATH=/mnt/afe01ad8-a763-4688-94a9-8827ed472dfa/flutter_projects/weather_fun"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=/mnt/afe01ad8-a763-4688-94a9-8827ed472dfa/flutterSdk/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
